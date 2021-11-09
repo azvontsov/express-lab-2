a00bbdc11cb028fc05699585b5fc29922271b79e
Anton <anton@Polinas-MacBook-Air.local>
2021-11-08 19:37:00 -0500

    delete if else

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
-const myName = [];
+
 
 
 app.get('/', (req, res) => {
     res.send('Hi, tis is the main page');
 })
 
-
 app.get('/greeting/:name', (req, res) => {
-  if (myName[req.params.name]) {
-     res.send(myName[req.params.name]); 
-  }else {
+
     res.send(
-      'What\'s up, ' + req.params.name + '! It\'s so great to see you!'
-      )
-  } 
-   
+        'What\'s up, ' + req.params.name + '! It\'s so great to see you!'
+    )
+
 });
+
+app.get('/tip', (req, res) => {
+
+})
+
 
 
 
